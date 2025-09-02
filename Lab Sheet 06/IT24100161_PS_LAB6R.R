2a145500b2adcdff6f7a7abaf721da1f9220b0e7
SethminFdo <it24100161@my.sliit.lk>
2025-09-02 10:12:08 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100161_PS_LAB6R.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\it24100161\\Desktop\\it24100161_ps_lab06")
+1
+A.
+#n=50
+#p=0.85
+B.
+1 - pbinom(46, size = 50, prob = 0.85)
+
+2.
+A.
+#X = the number of customer calls received in one hour 
+#    at the call center.
+
+B.
+#POISSON 12
+C.
+dpois(15, 12)
+
+
+
+
+
+ 
